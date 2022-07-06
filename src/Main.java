5f06dc44d5190d1f8d4e41ff8c361582bf24b21d
Daria Gromova <igromova81@gmail.com>
2022-07-06 21:29:19 +0300

    Задания 1-3. Строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1_2();
+        task3();
+    }
+    public static void task1_2() {
+        //Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        //Задание 2
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameUpperCase);
+    }
+        //Задание 3
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        String newFullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + newFullName);
     }
 }
